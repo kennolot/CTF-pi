9a51127cd6baeed43a52afc3417b3c560c83ab0e
kennolot <kennolot@users.noreply.github.com>
2025-02-28 13:17:46 +0000

    weaj filehost task completed

--- 4. vulnerable_web_apps/ctfpi4_3/app/app.py
+++ 4. vulnerable_web_apps/ctfpi4_3/app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
+import os
 app = Flask(__name__)
 
+os.makedirs('uploads', exist_ok=True)
+
 # Credits to: https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
-
-
 @app.route('/')
 def index():
     return '''    
@@ -13,6 +14,8 @@ def index():
         <title>Personal Raspi file hosting server</title>
     </head>
     <body>
+    <!-- user uploads go to /uploads -->
+    <!-- implement the admin directory auth later -->
         <h1>File Upload</h1>
         <form method="POST" action="" enctype="multipart/form-data">
         <p><input type="file" name="file"></p>
@@ -22,12 +25,46 @@ def index():
     </html>
 
     '''
+
+@app.route('/admin')
+def admin_uploads():
+    with open('flag.txt', 'r') as f:
+        read = f.read().strip()
+    return read
+
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return "File uploaded!"
+        file_path = os.path.join('uploads', uploaded_file.filename)
+        uploaded_file.save(file_path)
+    return "Submitted!"
+
+@app.route('/uploads')
+def uploads():
+    files = os.listdir('uploads')
+    files_html = ""
+    for file in files:
+        files_html += f'<li><a href="/uploads/{file}">{file}</a></li>'
+    
+    html = f'''
+    <!doctype html>
+    <html>
+    <head>
+        <title>Uploaded files</title>
+    </head>
+    <body>
+        <h1>Uploaded files</h1>
+        <ul> {files_html} </ul>
+        <p><a href="/">Upload more</a></p>
+    </body>
+    </html>
+    '''
+    return html
 
 
-app.run()
+@app.route('/uploads/<filename>')
+def download_file(filename):    
+    return send_from_directory('uploads', filename)
+
+app.run(host='0.0.0.0', port=8080, debug=True)
--- 4. vulnerable_web_apps/ctfpi4_3/manual4_3.md
+++ 4. vulnerable_web_apps/ctfpi4_3/manual4_3.md
@@ -2,7 +2,9 @@
 
 ### Scenario
 
-There's a 
+One of more common Raspberry Pi projects is a file server, your own "cloud" solution where you can upload files. This is also a good place for bad actors to breach and get access to your files.
+
+This particular application hosts your files locally, so anyone using the same network as the Pi can access the uploaded files.
 
 ### Prerequisites
 
@@ -12,7 +14,21 @@ There's a
 
 Be inside the current directory.
 
+`docker-compose up --build -d`
+
+`hostname -I` and go to http://<ip-address>:8080
+
+Find the flag hidden in the file hosting web app.
 
 
+### Cleaning up
 
+`docker-compose down`
+
+
+### **Hints**
+
+View the website's source code, there's a clue hidden there.
+
+The hidden flag directory is guessable, but tools like `gobuster` `dirbuster` etc can be used aswell.
 
