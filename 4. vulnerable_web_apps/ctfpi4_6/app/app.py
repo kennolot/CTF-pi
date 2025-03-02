888572fee5785bc4b204316969d10e2b7a573a2f
kennolot <kennolot@users.noreply.github.com>
2025-03-02 12:01:19 +0000

    4_6 is unfinished

---
+++ 4. vulnerable_web_apps/ctfpi4_6/app/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, send_file
+import os
+app = Flask(__name__)
+
+os.makedirs('uploads', exist_ok=True)
+
+# Credits to: https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
+@app.route('/')
+def index():    
+    
+    return f'''    
+    <!doctype html>
+    <html>
+    <head>
+        <title>Personal Raspi file hosting server</title>
+    </head>
+    <body>        
+        <h1>File Upload</h1>
+        <form method="POST" action="" enctype="multipart/form-data">
+        <p><input type="file" name="file"></p>
+        <p><input type="submit" value="Submit"></p>
+        </form>
+        
+    </body>
+    </html>
+
+    '''
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        file_path = os.path.join('uploads', uploaded_file.filename)
+        uploaded_file.save(file_path)
+    return "Submitted!"
+
+@app.route('/uploads')
+def uploads():
+    files = os.listdir('uploads')
+    files_html = ""
+    for file in files:
+        files_html += f'<li><a href="/uploads/{file}">{file}</a></li>'
+    
+    html = f'''
+    <!doctype html>
+    <html>
+    <head>
+        <title>Uploaded files</title>
+    </head>
+    <body>
+        <h1>Uploaded files</h1>
+        <ul> {files_html} </ul>
+        <p><a href="/">Upload more</a></p>
+    </body>
+    </html>
+    '''
+    return html
+
+
+@app.route('/uploads/<filename>')
+def download_file(filename):        
+    # i made this so user can read directly inside the app instead of downloading
+    output = os.popen(f'cat uploads/{filename}').read()
+    return output
+
+app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ 4. vulnerable_web_apps/ctfpi4_6/manual4_6.md
@@ -0,0 +1,40 @@
+## Weak file hosting 4
+
+### Scenario
+
+The previous web app developer was notified and is currently fixing their app issues. 
+
+His friend however copied his code and made a few changes, take a peek at `app/app.py` and see what has changed with `ctfpi4_5/app/app.py`.
+
+### Prerequisites
+
+1x Raspberry Pi
+
+### Requirements 
+
+Be inside the current directory.
+
+`docker-compose up --build -d`
+
+`hostname -I` and go to http://<ip-address>:8080 from Pi remote connection
+or a machine that's connected to the same local network where Pi hosts the webapp
+
+Find the flag hidden in the file hosting web app.
+
+
+### Cleaning up
+
+`docker-compose down`
+
+
+### **Hints**
+
+Check out the `app/app.py` for source code.
+
+Are user inputs sanitized?
+
+Upload a file named `pwd` for example, in linux terminal this would print the current active directory. What's happening here?
+
+`;` escapes the command.
+
+Path traversal to find the flag.
