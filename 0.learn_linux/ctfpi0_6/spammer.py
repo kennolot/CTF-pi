759b5f934a5b8035a1cdf59b0e528c2f036450b0
kennolot <kennolot@users.noreply.github.com>
2025-03-28 16:45:44 +0000

    minor tweaks

--- 0.learn_linux/ctfpi0_6/spammer.py
+++ 0.learn_linux/ctfpi0_6/spammer.py
@@ -1,12 +1,9 @@
 import time
 
 message = '22'
-message2 = '333'
 
 while True:
 	time.sleep(0.1)
-	with open("answers/flag_encoded.txt", "a") as f:
+	with open("answers/flag_encoded.txt", "w") as f:
 		f.write(message)
-	with open("answers/flag_encoded.txt", "a") as f:
-                f.write(message2)
 
